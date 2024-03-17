abb0095ed5a95bcbd91f820c6a6b73a95c12ea75
MD. SHAHIDUL ISLAM <11449967+xeron56@users.noreply.github.com>
2024-03-17 01:39:22 +0000

    pre nullsafe fix

--- lib/generation/prototyping/pb_prototype_linker_service.dart
+++ lib/generation/prototyping/pb_prototype_linker_service.dart
@@ -1,11 +1,12 @@
 import 'package:parabeac_core/generation/prototyping/pb_prototype_aggregation_service.dart';
 import 'package:parabeac_core/generation/prototyping/pb_prototype_storage.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/inherited_scaffold.dart';
-import 'package:parabeac_core/interpret_and_optimize/entities/interfaces/pb_prototype_enabled.dart';
+//import 'package:parabeac_core/interpret_and_optimize/entities/interfaces/pb_prototype_enabled.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_node.dart';
 import 'package:parabeac_core/interpret_and_optimize/helpers/pb_context.dart';
 import 'package:parabeac_core/interpret_and_optimize/helpers/pb_intermediate_node_tree.dart';
 import 'package:parabeac_core/interpret_and_optimize/services/intermediate_node_searcher_service.dart';
+import 'package:parabeac_core/interpret_and_optimize/entities/interfaces/pb_prototype_enabled.dart';
 
 class PBPrototypeLinkerService {
   PBPrototypeStorage _prototypeStorage;
@@ -22,16 +23,32 @@ class PBPrototypeLinkerService {
     if (rootNode == null) {
       return rootNode;
     }
+    // for (var element in tree) {
+    //   if (element is InheritedScaffold) {
+    //     await _prototypeStorage.addPageNode(element, context);
+    //   } else if (element is PrototypeEnable) {
+
+    //     if (element.prototypeNode?.destinationUUID != null &&
+    //         (element).prototypeNode.destinationUUID.isNotEmpty) {
+    //       await addAndPopulatePrototypeNode(element, rootNode, context);
+    //     }
+    //   }
+    // }
     for (var element in tree) {
       if (element is InheritedScaffold) {
         await _prototypeStorage.addPageNode(element, context);
       } else if (element is PrototypeEnable) {
-        if (element.prototypeNode?.destinationUUID != null &&
-            (element).prototypeNode.destinationUUID.isNotEmpty) {
-          await addAndPopulatePrototypeNode(element, rootNode, context);
+        // Ensure element is treated as PrototypeEnable
+        var prototypeEnableElement =
+            element as PrototypeEnable; // Explicit cast
+        if (prototypeEnableElement.prototypeNode?.destinationUUID != null &&
+            prototypeEnableElement.prototypeNode.destinationUUID.isNotEmpty) {
+          await addAndPopulatePrototypeNode(
+              prototypeEnableElement, rootNode, context);
         }
       }
     }
+
     return rootNode;
   }
 
--- lib/interpret_and_optimize/state_management/directed_state_graph.dart
+++ lib/interpret_and_optimize/state_management/directed_state_graph.dart
@@ -4,21 +4,24 @@ import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_inte
 /// [DirectedStateGraph] is used to represent the states of a
 /// `default state management node`.
 class DirectedStateGraph extends DirectedGraph<PBIntermediateNode> {
-
   /// `defaultNode` is considered to be the starting point for this graph.
   ///
   /// Any other [PBIntermediateNode] that is pointed to by `defaultNode` is considered to be
   /// a `variation` of the `defaultNode`.
   PBIntermediateNode defaultNode;
 
+  // DirectedStateGraph(
+  //   this.defaultNode, {
+  //   Map<Vertex<PBIntermediateNode>, List<Vertex<PBIntermediateNode>>> edges,
+  // }) : super(edges);
   DirectedStateGraph(
-    this.defaultNode, {
-    Map<Vertex<PBIntermediateNode>, List<Vertex<PBIntermediateNode>>> edges,
-  }) : super(edges);
+  this.defaultNode, {
+  Map<PBIntermediateNode, Set<PBIntermediateNode>> edges,
+}) : super(edges);
 
   /// Adds `variation` as a state of `defaultNode`
   void addVariation(PBIntermediateNode variation) =>
-      super.addEdges(defaultNode, [variation]);
+      super.addEdges(defaultNode, {variation});
 
   /// Retrieves the states of `defaultNode`
   List<PBIntermediateNode> get states =>
