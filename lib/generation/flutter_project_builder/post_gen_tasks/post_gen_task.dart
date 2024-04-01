3a0c97cc504f887e06482c69535d60c0ea9c28d2
xeron56 <shahidul1425@cseku.ac.bd>
2024-04-01 18:12:55 +0600

    generate null safe binding

--- lib/generation/flutter_project_builder/post_gen_tasks/post_gen_task.dart
+++
@@ -1,7 +0,0 @@
-import 'package:parabeac_core/controllers/main_info.dart';
-
-/// Abstract class for Tasks that will run post-generation.
-abstract class PostGenTask {
-  /// Executes the [PostGenTask].
-  void execute();
-}
